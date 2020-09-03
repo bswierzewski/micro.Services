8a89fea3af8a84e0d2a0119848953fed663e2f4a
b.swierzewski <b.swierzewski@7technology.pl>
2020-09-03 10:51:15 +0200

    Add try catch

--- Device/Controllers/DevicesController.cs
+++ Device/Controllers/DevicesController.cs
@@ -26,95 +26,161 @@ namespace Device.Controllers
         [HttpGet("{id}")]
         public async Task<IActionResult> GetDeviceDto(int id)
         {
-            var device = await _repo.GetDevices(expressionDevice: n => n.Id == id).FirstOrDefaultAsync();
+            try
+            {
+                var device = await _repo.GetDevices(expressionDevice: n => n.Id == id).FirstOrDefaultAsync();
 
-            return Ok(device);
+                return Ok(device);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+
+                return StatusCode((int)HttpStatusCode.InternalServerError, "Error!");
+            }
         }
 
         [HttpGet]
         public async Task<IActionResult> GetDevicesDto()
         {
-            var devices = await _repo.GetDevices().ToListAsync();
+            try
+            {
+                var devices = await _repo.GetDevices().ToListAsync();
 
-            return Ok(devices);
+                return Ok(devices);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+
+                return StatusCode((int)HttpStatusCode.InternalServerError, "Error!");
+            }
         }
 
         [HttpGet("categories/{id}")]
         public async Task<IActionResult> GetDeviceDtoByCategory(int id)
         {
-            var device = await _repo.GetDevices(expressionDevice: n => n.Component.CategoryId == id).ToListAsync();
+            try
+            {
+                var device = await _repo.GetDevices(expressionDevice: n => n.Component.CategoryId == id).ToListAsync();
 
-            return Ok(device);
+                return Ok(device);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+
+                return StatusCode((int)HttpStatusCode.InternalServerError, "Error!");
+            }
         }
 
         [HttpGet("components/{id}")]
         public async Task<IActionResult> GetDeviceDtoByComponent(int id)
         {
-            var device = await _repo.GetDevices(expressionDevice: n => n.ComponentId == id).ToListAsync();
+            try
+            {
+                var device = await _repo.GetDevices(expressionDevice: n => n.ComponentId == id).ToListAsync();
 
-            return Ok(device);
+                return Ok(device);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+
+                return StatusCode((int)HttpStatusCode.InternalServerError, "Error!");
+            }
         }
 
         [HttpGet("kinds/{id}")]
         public async Task<IActionResult> GetDeviceDtoByKind(int id)
         {
-            var device = await _repo.GetDevices(expressionDevice: n => n.KindId == id).ToListAsync();
+            try
+            {
+                var device = await _repo.GetDevices(expressionDevice: n => n.KindId == id).ToListAsync();
 
-            return Ok(device);
+                return Ok(device);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+
+                return StatusCode((int)HttpStatusCode.InternalServerError, "Error!");
+            }
         }
 
         [HttpPost("add")]
         public async Task<IActionResult> AddDevice(AddDeviceDto addDeviceDto)
         {
-            if (await _repo.ExistsDevice(addDeviceDto.MacAddress))
-                return StatusCode((int)HttpStatusCode.BadRequest, "Device exists!");
-
-            var newDevice = new Database.Entities.Device()
+            try
             {
-                MacAddress = addDeviceDto.MacAddress,
-                Name = addDeviceDto.Name,
-                Created = DateTime.Now,
-                KindId = addDeviceDto.KindId,
-                ComponentId = addDeviceDto.ComponentId,
-                PhotoUrl = addDeviceDto.PhotoUrl,
-                SpecificVersionId = addDeviceDto.SpecificVersionId,
-            };
+                if (await _repo.ExistsDevice(addDeviceDto.MacAddress))
+                    return StatusCode((int)HttpStatusCode.BadRequest, "Device exists!");
 
-            await _repo.Add(newDevice);
+                var newDevice = new Database.Entities.Device()
+                {
+                    MacAddress = addDeviceDto.MacAddress,
+                    Name = addDeviceDto.Name,
+                    Created = DateTime.Now,
+                    KindId = addDeviceDto.KindId,
+                    ComponentId = addDeviceDto.ComponentId,
+                    PhotoUrl = addDeviceDto.PhotoUrl,
+                    SpecificVersionId = addDeviceDto.SpecificVersionId,
+                };
 
-            return Ok(newDevice);
+                await _repo.Add(newDevice);
+
+                return Ok(newDevice);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+
+                return StatusCode((int)HttpStatusCode.InternalServerError, "Error!");
+            }
         }
 
         [HttpPost("{id}/update")]
         public async Task<IActionResult> UpdateDeviceKind(int id, UpdateDeviceDto updateDeviceDto)
         {
-            var device = await _repo.GetDevice(id);
+            try
+            {
+                var device = await _repo.GetDevice(id);
 
-            if (device == null)
-                return StatusCode((int)HttpStatusCode.BadRequest, "Device not exists!");
+                if (device == null)
+                    return StatusCode((int)HttpStatusCode.BadRequest, "Device not exists!");
 
-            if (!string.IsNullOrEmpty(updateDeviceDto.Name))
-                device.Name = updateDeviceDto.Name;
+                if (!string.IsNullOrEmpty(updateDeviceDto.Name))
+                    device.Name = updateDeviceDto.Name;
 
-            if ((updateDeviceDto.SpecificVersionId ?? 0) > 0)
-                device.SpecificVersionId = updateDeviceDto.SpecificVersionId;
+                if (updateDeviceDto.SpecificVersionId.HasValue)
+                    device.SpecificVersionId = updateDeviceDto.SpecificVersionId;
 
-            if ((updateDeviceDto.KindId ?? 0) > 0)
-                device.KindId = updateDeviceDto.KindId;
+                if (updateDeviceDto.KindId.HasValue)
+                    device.KindId = updateDeviceDto.KindId;
 
-            if ((updateDeviceDto.ComponentId ?? 0) > 0)
-                device.ComponentId = updateDeviceDto.ComponentId;
+                if (updateDeviceDto.ComponentId.HasValue)
+                    device.ComponentId = updateDeviceDto.ComponentId;
 
-            if (!string.IsNullOrEmpty(updateDeviceDto.PhotoUrl))
-                device.PhotoUrl = updateDeviceDto.PhotoUrl;
+                if (!string.IsNullOrEmpty(updateDeviceDto.PhotoUrl))
+                    device.PhotoUrl = updateDeviceDto.PhotoUrl;
 
-            device.Modified = DateTime.Now;
+                if (updateDeviceDto.IsAutoUpdate.HasValue)
+                    device.IsAutoUpdate = updateDeviceDto.IsAutoUpdate;
 
-            await _repo.SaveAllChanges();
+                device.Modified = DateTime.Now;
 
-            var result = await _repo.GetDevices(x => x.Id == id).FirstOrDefaultAsync();
+                await _repo.SaveAllChanges();
 
-            return Ok(result);
+                var result = await _repo.GetDevices(x => x.Id == id).FirstOrDefaultAsync();
+
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+
+                return StatusCode((int)HttpStatusCode.InternalServerError, "Error!");
+            }
         }
     }
 }
