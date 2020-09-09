2df53235b98ecd0bd890f750c6caa76b787159d8
b.swierzewski <b.swierzewski@7technology.pl>
2020-09-09 15:53:13 +0200

    Add Icons StaticData

--- JsonDbCreator/Program.cs
+++ JsonDbCreator/Program.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿using Bogus;
+using Database.Entities;
+using Database.Entities.DeviceInfo;
+using JsonDbCreator.StaticData;
+using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Linq;
 
 namespace JsonDbCreator
 {
@@ -6,6 +13,35 @@ namespace JsonDbCreator
     {
         static void Main(string[] args)
         {
+            int categoryCounter = 0;
+
+            var categoryFaker = new Faker<Category>()
+                .RuleFor(x => x.Id, x => categoryCounter++)
+                .RuleFor(x => x.Created, x => x.Date.Soon())
+                .RuleFor(x => x.Name, x => x.Name.FirstName())
+                .RuleFor(x => x.FontAwesome, x => x.PickRandom(Icons.FontAwesome));
+
+            var categories = categoryFaker.Generate(2);
+
+            int componentCounter = 0;
+
+            var componentFaker = new Faker<Component>()
+                .RuleFor(x => x.Id, x => componentCounter++)
+                .RuleFor(x => x.Created, x => x.Date.Soon())
+                .RuleFor(x => x.Name, x => x.Name.FirstName())
+                .RuleFor(x => x.FontAwesome, x => x.PickRandom(Icons.FontAwesome));
+
+            var components = componentFaker.Generate(10);
+
+            var json = JsonConvert.SerializeObject(new
+            {
+                categories = categories,
+                components = components
+            }, Formatting.Indented);
+
+            //File.WriteAllText("db.json", json);
+
+            Console.WriteLine(json);
         }
     }
 }
---
+++ JsonDbCreator/StaticData/Icons.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JsonDbCreator.StaticData
+{
+    public static class Icons
+    {
+        public static string[] MaterialDesign = {
+            "3d_rotation", "accessibility", "accessibility_new", "accessible","accessible_forward", "account_balance", "account_balance_wallet","account_box", "account_circle",
+            "add_shopping_cart", "alarm", "alarm_add","alarm_off", "alarm_on", "all_out", "android", "announcement","arrow_right_alt", "aspect_ratio", "assessment",
+            "assignment", "assignment_ind","assignment_late", "assignment_return", "assignment_returned",
+            "assignment_turned_in", "autorenew", "backup", "book", "bookmark","bookmark_border", "bug_report", "build", "cached", "calendar_today",
+            "calendar_view_day", "camera_enhance", "card_giftcard", "card_membership","card_travel", "change_history", "check_circle", "check_circle_outline",
+            "chrome_reader_mode", "class", "code", "commute", "compare_arrows","contact_support", "copyright", "credit_card", "dashboard", "date_range",
+            "delete", "delete_forever", "delete_outline", "description", "dns", "done","done_all", "done_outline", "donut_large", "donut_small", "drag_indicator",
+            "eject", "euro_symbol", "event", "event_seat", "exit_to_app", "explore","extension", "face", "favorite", "favorite_border", "feedback", "find_in_page",
+            "find_replace", "fingerprint", "flight_land", "flight_takeoff", "flip_to_back","flip_to_front", "g_translate", "gavel", "get_app", "gif", "grade",
+            "group_work", "help", "help_outline", "highlight_off", "history", "home","horizontal_split", "hourglass_empty", "hourglass_full", "http", "https",
+            "important_devices", "info", "input", "invert_colors", "label","label_important", "language", "launch", "line_style", "line_weight", "list",
+            "lock", "lock_open", "loyalty", "markunread_mailbox", "maximize", "minimize","motorcycle", "note_add", "offline_bolt", "offline_pin", "opacity",
+            "open_in_browser", "open_in_new", "open_with", "pageview", "pan_tool","payment", "perm_camera_mic", "perm_contact_calendar", "perm_data_setting",
+            "perm_device_information", "perm_identity", "perm_media", "perm_phone_msg","perm_scan_wifi", "pets", "picture_in_picture", "picture_in_picture_alt",
+            "play_for_work", "polymer", "power_settings_new", "pregnant_woman", "print","query_builder", "question_answer", "receipt", "record_voice_over", "redeem",
+            "remove_shopping_cart", "reorder", "report_problem", "restore","restore_from_trash", "restore_page", "room", "rounded_corner", "rowing",
+            "schedule", "search", "settings", "settings_applications","settings_backup_restore", "settings_bluetooth", "settings_brightness",
+            "settings_cell", "settings_ethernet", "settings_input_antenna","settings_input_component", "settings_input_composite", "settings_input_hdmi",
+            "settings_input_svideo", "settings_overscan", "settings_phone","settings_power", "settings_remote", "settings_voice", "shop", "shop_two",
+            "shopping_basket", "shopping_cart", "speaker_notes", "speaker_notes_off","spellcheck", "stars", "store", "subject", "supervised_user_circle",
+            "supervisor_account", "swap_horiz", "swap_horizontal_circle", "swap_vert","swap_vertical_circle", "tab", "tab_unselected", "text_rotate_up",
+            "text_rotate_vertical", "text_rotation_down", "text_rotation_none", "theaters","thumb_down", "thumb_up", "thumbs_up_down", "timeline", "toc", "today", "toll",
+            "touch_app", "track_changes", "translate", "trending_down", "trending_flat","trending_up", "turned_in", "turned_in_not", "update", "verified_user",
+            "vertical_split", "view_agenda", "view_array", "view_carousel", "view_column","view_day", "view_headline", "view_list", "view_module", "view_quilt",
+            "view_stream", "view_week", "visibility", "visibility_off", "voice_over_off","watch_later", "work", "work_off", "work_outline", "youtube_searched_for",
+            "zoom_in", "zoom_out"
+        };
+
+        public static string[] FontAwesome = { "fa fa-glass", "fa fa-music", "fa fa-search", "fa fa-envelope-o", "fa fa-heart", "fa fa-star", "fa fa-star-o", "fa fa-user", "fa fa-film",
+            "fa fa-th-large", "fa fa-th", "fa fa-th-list", "fa fa-check", "fa fa-times", "fa fa-search-plus", "fa fa-search-minus", "fa fa-power-off", "fa fa-signal", "fa fa-cog",
+            "fa fa-trash-o", "fa fa-home", "fa fa-file-o", "fa fa-clock-o", "fa fa-road", "fa fa-download", "fa fa-arrow-circle-o-down", "fa fa-arrow-circle-o-up", "fa fa-inbox",
+            "fa fa-play-circle-o", "fa fa-repeat", "fa fa-refresh", "fa fa-list-alt", "fa fa-lock", "fa fa-flag", "fa fa-headphones", "fa fa-volume-off", "fa fa-volume-down",
+            "fa fa-volume-up", "fa fa-qrcode", "fa fa-barcode", "fa fa-tag", "fa fa-tags", "fa fa-book", "fa fa-bookmark", "fa fa-print", "fa fa-camera", "fa fa-font", "fa fa-bold",
+            "fa fa-italic", "fa fa-text-height", "fa fa-text-width", "fa fa-align-left", "fa fa-align-center", "fa fa-align-right", "fa fa-align-justify", "fa fa-list", "fa fa-outdent",
+            "fa fa-indent", "fa fa-video-camera", "fa fa-picture-o", "fa fa-pencil", "fa fa-map-marker", "fa fa-adjust", "fa fa-tint", "fa fa-pencil-square-o", "fa fa-share-square-o",
+            "fa fa-check-square-o", "fa fa-arrows", "fa fa-step-backward", "fa fa-fast-backward", "fa fa-backward", "fa fa-play", "fa fa-pause", "fa fa-stop", "fa fa-forward",
+            "fa fa-fast-forward", "fa fa-step-forward", "fa fa-eject", "fa fa-chevron-left", "fa fa-chevron-right", "fa fa-plus-circle", "fa fa-minus-circle", "fa fa-times-circle",
+            "fa fa-check-circle", "fa fa-question-circle", "fa fa-info-circle", "fa fa-crosshairs", "fa fa-times-circle-o", "fa fa-check-circle-o", "fa fa-ban", "fa fa-arrow-left",
+            "fa fa-arrow-right", "fa fa-arrow-up", "fa fa-arrow-down", "fa fa-share", "fa fa-expand", "fa fa-compress", "fa fa-plus", "fa fa-minus", "fa fa-asterisk",
+            "fa fa-exclamation-circle", "fa fa-gift", "fa fa-leaf", "fa fa-fire", "fa fa-eye", "fa fa-eye-slash", "fa fa-exclamation-triangle", "fa fa-plane", "fa fa-calendar",
+            "fa fa-random", "fa fa-comment", "fa fa-magnet", "fa fa-chevron-up", "fa fa-chevron-down", "fa fa-retweet", "fa fa-shopping-cart", "fa fa-folder", "fa fa-folder-open",
+            "fa fa-arrows-v", "fa fa-arrows-h", "fa fa-bar-chart", "fa fa-twitter-square", "fa fa-facebook-square", "fa fa-camera-retro", "fa fa-key", "fa fa-cogs", "fa fa-comments",
+            "fa fa-thumbs-o-up", "fa fa-thumbs-o-down", "fa fa-star-half", "fa fa-heart-o", "fa fa-sign-out", "fa fa-linkedin-square", "fa fa-thumb-tack", "fa fa-external-link",
+            "fa fa-sign-in", "fa fa-trophy", "fa fa-github-square", "fa fa-upload", "fa fa-lemon-o", "fa fa-phone", "fa fa-square-o", "fa fa-bookmark-o", "fa fa-phone-square",
+            "fa fa-twitter", "fa fa-facebook", "fa fa-github", "fa fa-unlock", "fa fa-credit-card", "fa fa-rss", "fa fa-hdd-o", "fa fa-bullhorn", "fa fa-bell", "fa fa-certificate",
+            "fa fa-hand-o-right", "fa fa-hand-o-left", "fa fa-hand-o-up", "fa fa-hand-o-down", "fa fa-arrow-circle-left", "fa fa-arrow-circle-right", "fa fa-arrow-circle-up",
+            "fa fa-arrow-circle-down", "fa fa-globe", "fa fa-wrench", "fa fa-tasks", "fa fa-filter", "fa fa-briefcase", "fa fa-arrows-alt", "fa fa-users", "fa fa-link", "fa fa-cloud",
+            "fa fa-flask", "fa fa-scissors", "fa fa-files-o", "fa fa-paperclip", "fa fa-floppy-o", "fa fa-square", "fa fa-bars", "fa fa-list-ul", "fa fa-list-ol", "fa fa-strikethrough",
+            "fa fa-underline", "fa fa-table", "fa fa-magic", "fa fa-truck", "fa fa-pinterest", "fa fa-pinterest-square", "fa fa-google-plus-square", "fa fa-google-plus", "fa fa-money",
+            "fa fa-caret-down", "fa fa-caret-up", "fa fa-caret-left", "fa fa-caret-right", "fa fa-columns", "fa fa-sort", "fa fa-sort-desc", "fa fa-sort-asc", "fa fa-envelope",
+            "fa fa-linkedin", "fa fa-undo", "fa fa-gavel", "fa fa-tachometer", "fa fa-comment-o", "fa fa-comments-o", "fa fa-bolt", "fa fa-sitemap", "fa fa-umbrella", "fa fa-clipboard",
+            "fa fa-lightbulb-o", "fa fa-exchange", "fa fa-cloud-download", "fa fa-cloud-upload", "fa fa-user-md", "fa fa-stethoscope", "fa fa-suitcase", "fa fa-bell-o", "fa fa-coffee",
+            "fa fa-cutlery", "fa fa-file-text-o", "fa fa-building-o", "fa fa-hospital-o", "fa fa-ambulance", "fa fa-medkit", "fa fa-fighter-jet", "fa fa-beer", "fa fa-h-square", "fa fa-plus" };
+    }
+}
