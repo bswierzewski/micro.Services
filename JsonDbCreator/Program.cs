f02bd52c91c62f4fa18aac90333eb7a8867fa508
b.swierzewski <b.swierzewski@7technology.pl>
2020-09-10 08:16:07 +0200

    Add desktop as default db.json file create

--- JsonDbCreator/Program.cs
+++ JsonDbCreator/Program.cs
@@ -1,11 +1,10 @@
 ﻿using Bogus;
-using Database.Entities;
 using Database.Entities.DeviceInfo;
 using JsonDbCreator.StaticData;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 using System;
 using System.IO;
-using System.Linq;
 
 namespace JsonDbCreator
 {
@@ -13,46 +12,50 @@ namespace JsonDbCreator
     {
         static void Main(string[] args)
         {
-            int categoryCounter = 0;
+            int counter = 0;
 
             var categoryFaker = new Faker<Category>()
-                .RuleFor(x => x.Id, f => categoryCounter++)
+                .RuleFor(x => x.Id, f => counter++)
                 .RuleFor(x => x.Created, f => f.Date.Soon())
                 .RuleFor(x => x.Name, f => f.Name.FirstName())
-                .RuleFor(x => x.IconName, f => f.PickRandom(Icons.FontAwesome));
+                .RuleFor(x => x.IconName, f => f.PickRandom(Icons.MaterialDesign));
 
             var categories = categoryFaker.Generate(10);
-
-            int componentCounter = 0;
+            counter = 0;
 
             var componentFaker = new Faker<Component>()
-                .RuleFor(x => x.Id, f => componentCounter++)
+                .RuleFor(x => x.Id, f => counter++)
                 .RuleFor(x => x.Created, f => f.Date.Soon())
                 .RuleFor(x => x.Name, f => f.Name.FirstName())
-                .RuleFor(x => x.IconName, f => f.PickRandom(Icons.FontAwesome));
+                .RuleFor(x => x.IconName, f => f.PickRandom(Icons.MaterialDesign));
 
             var components = componentFaker.Generate(20);
-
-            int kindCounter = 0;
+            counter = 0;
 
             var kindFaker = new Faker<Kind>()
-                .RuleFor(x => x.Id, f => kindCounter++)
+                .RuleFor(x => x.Id, f => counter++)
                 .RuleFor(x => x.Created, f => f.Date.Soon())
                 .RuleFor(x => x.Name, f => f.Name.FirstName())
                 .RuleFor(x => x.PhotoUrl, f => f.Image.PicsumUrl());
 
             var kinds = kindFaker.Generate(10);
-
-
+            counter = 0;
 
             var json = JsonConvert.SerializeObject(new
             {
                 categories = categories,
                 components = components,
                 kinds = kinds
-            }, Formatting.Indented);
+            }, new JsonSerializerSettings()
+            {
+                Formatting = Formatting.Indented,
+                ContractResolver = new DefaultContractResolver()
+                {
+                    NamingStrategy = new CamelCaseNamingStrategy()
+                }
+            });
 
-            File.WriteAllText("db.json", json);
+            File.WriteAllText(Path.Join(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "db.json"), json);
 
             //Console.WriteLine(json);
         }
